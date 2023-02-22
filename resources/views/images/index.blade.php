53302aa4e87928703ca6c0a60611f993c136f935
Ismail Cetin <isocetin1907@gmail.com>
2023-02-22 16:42:30 +0100

    Image setup

---
+++ resources/views/images/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Image Uploading with Vue + Laravel</title>
+</head>
+
+<body>
+    <div class="max-w-lg mx-auto mt-24">
+        <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+    </div>
+</body>
+
+</html>
