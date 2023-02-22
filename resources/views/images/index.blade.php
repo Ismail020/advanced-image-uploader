10d1d97219ff9101ea495b63de4cf28c9b6815a5
Ismail Cetin <isocetin1907@gmail.com>
2023-02-22 17:01:43 +0100

    Vue 3 init

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -6,12 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Image Uploading with Vue + Laravel</title>
+
+    @vite('resources/css/app.css')
 </head>
 
 <body>
     <div class="max-w-lg mx-auto mt-24">
         <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+        <div id="app">
+
+        </div>
     </div>
+    @vite('resources/js/app.js')
 </body>
 
 </html>
